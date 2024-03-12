46bac57daf6610b4ad5c229f7453b32235adeed9
guille <guille@ejemplo.es>
2024-03-12 16:32:36 +0100

    API casi terminada

--- src/main/java/org/iesvdm/clubjava/controller/TagController.java
+++ src/main/java/org/iesvdm/clubjava/controller/TagController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Slf4j
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/v1/api/tags")
 public class TagController {
